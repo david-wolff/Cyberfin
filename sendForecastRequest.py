75b4dc17f3749f6fcf58c5a32f8b53867b45c1c8
David Wolff <davidwolff@Davids-MacBook-Air.local>
2024-03-25 23:29:36 -0300

    Implementing daily data check and CSS.

--- sendForecastRequest.py
+++ sendForecastRequest.py
@@ -1,27 +1,30 @@
 import json
-import requests 
+import requests
 import os
 import geocoder
+from datetime import datetime
 from dotenv import load_dotenv
-from geopy.geocoders import Nominatim
 
 load_dotenv()
 
+def update_needed(json_file_path):
+    try:
+        with open(json_file_path, 'r') as f:
+            data = json.load(f)
+            latest_date = max(datetime.strptime(entry['time'], "%Y-%m-%dT%H:%M:%S+00:00") for entry in data['hours'])
+            return datetime.now() > latest_date
+    except (FileNotFoundError, ValueError, KeyError, json.JSONDecodeError):
+        return True
+
 def get_and_store_forecast(output_file): 
-    
     location = geocoder.ip('me')
-    lat, lng = location.latlng  # Get the latitude and longitude
+    lat, lng = location.latlng  
     lat = round(lat, 3) 
-    lng = round(lng, 3)  # Round the longitude to 3 decimal places
-    #geolocator = Nominatim(user_agent = "geoapiExercises")
-    #location = geolocator.geocode(location_name)
-    #lat, lon = round(location.latitude, 3), round(location.longitude, 3)
+    lng = round(lng, 3)  
     params = 'waterTemperature,wavePeriod,waveDirection,windDirection,windSpeed,waveHeight'
     api_url = f"https://api.stormglass.io/v2/weather/point?lat={lat}&lng={lng}&params={params}"
     headers = {
-        
-        'Authorization' : os.getenv('STORMGLASS_API_KEY')
-    
+        'Authorization': os.getenv('STORMGLASS_API_KEY')
     }
     
     response = requests.get(api_url, headers=headers)
@@ -31,8 +34,13 @@ def get_and_store_forecast(output_file):
         
         with open(output_file, 'w') as file: 
             json.dump(data, file, indent=4)
-        print(f"Forecast data for {location} has been saved to {output_file}") 
+        print(f"Forecast data has been saved to {output_file}") 
     else:
         print(f"Failed to get forecast data: {response.status_code}")
+
 if __name__ == "__main__":
-    get_and_store_forecast("forecast_data.json")
+    json_file_path = "forecast_data.json"
+    if update_needed(json_file_path):
+        get_and_store_forecast(json_file_path)
+    else:
+        print("No update needed. Forecast data is up-to-date.")
